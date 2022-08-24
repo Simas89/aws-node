2476c98c644e426bdf2b6e6a1c895462219b12c0
Simas <simaszurauskas@gmail.com>
2022-08-24 18:49:07 +0300

    dev

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 
 app.get("/", (req, res) => res.send("Hi there :)"));
 
-const PORT = process.env.port || 4000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log("Hey!");
 });
